6c526d6dd765e92908c5fcb8760b464f7e1f6971
Arturo Rodríguez Vázquez <arturo.rodriguezvqz@segurossura.com.mx>
2024-05-30 05:39:13 -0600

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+// ejecutar prisma npx prisma init
+
+// ejecutamos la micración de prisma para que lo haga por nosotros en sql poniendo la sentencia
+// npx prisma migrate dev --name init
+
+// ejecutamos también el cliente de prisma
+// npm install @prisma/client 
+
+async function bootstrap() {
+
+  const logger = new Logger('ProductsMS-main');
+  
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      }
+    }
+  );
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+
+  // await app.listen(envs.port);
+  await app.listen();
+
+  logger.log(`Products Microservice running on port ${ envs.port }`);
+}
+bootstrap();
