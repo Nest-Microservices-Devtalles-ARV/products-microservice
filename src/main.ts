9059c169539b9978d1b42c0af0c9b98887f1b5ed
Arturo Rodríguez Vázquez <arturo.rodriguezvqz@segurossura.com.mx>
2024-07-14 18:48:30 -0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,11 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      // transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        // port: envs.port,
+        servers: envs.natsServers
       }
     }
   );
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit{
@@ -49,7 +50,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
     });
 
     if( !product ){
-      throw new NotFoundException(`Product with id ${ id } not found`);
+      // throw new NotFoundException(`Product with id ${ id } not found`); // NO esta mal pero para microservicios es mejor Rpc
+      // throw new RpcException(`Product with id ${ id } not found`);
+      throw new RpcException({
+        message: `Product with id ${ id } not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
 
     return product;
@@ -83,5 +89,29 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
     });
 
     return product;
+  }
+
+  async validateProducts(ids: number[]){
+
+    // Borrar duplicados
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if( products.length != ids.length ){
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+
   }
 }
